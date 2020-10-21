913ca5c58322b8ef27752cb6636584211600d09f
ghemsley <ghemsley@protonmail.ch>
2020-10-21 07:19:14 -0500

    commit tree 1fea3477b19a0869e6669d350689c38147b9dfb3

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  index = 0
+  new_array = []
+  array.each do |element|
+    new_array[index] = element**2
+    index += 1
+  end
+  return new_array
 end
